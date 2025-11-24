57d4210d702a6a589bff085e89a5750ba23336ce
knowave <kwaktaemin98@gmail.com>
2025-11-24 20:40:10 +0900

    UPDATE: 불필요한 버전 명시 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
-    implementation("org.springframework.boot:spring-boot-starter-validation:3.5.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
 
